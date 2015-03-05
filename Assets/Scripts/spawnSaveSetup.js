fc1e18bc21c089c6374887f01772eb8b8d4ca203
Nohm13 <nolan@theohms.com>
2015-03-05 09:54:43 -0800

    Change
    Added killBox and moved assets

---
+++ Assets/Scripts/spawnSaveSetup.js
@@ -0,0 +1,61 @@
+﻿#pragma strict
+
+var start : Transform;
+
+var soundRate : float = 0.0;
+var soundDelay : float = 0.0;
+
+var killTime : float = 5.0;
+var Lives : int = 3;
+function PlaySound(soundName,soundDelay : float)
+{
+	if (!audio.isPlaying && Time.time > soundRate)
+	{
+		soundRate = Time.time + soundDelay;
+		audio.clip = soundName;
+		audio.Play();
+	}
+}
+
+function Start () {
+
+}
+function Update () {
+	if(Lives == 0)
+		GetComponent(HpScript).gameOver = true;
+
+}
+function OnTriggerEnter(other : Collider)
+{
+	
+	if (other.tag == "killbox")
+	{
+		
+	}
+	if (other.tag == "DamageBlock")
+	{
+		GetComponent(HpScript).Hp-=5;
+	}
+}
+function OnTriggerStay(other : Collider)
+{
+	if(other.tag == "CamKill")
+	{
+		print("In");
+		GetComponent(HpScript).Hp-=.4;
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
