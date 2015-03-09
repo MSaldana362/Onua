c03901e2e6eb14c6039cbd9b71febac629fd219a
davidb2277 <davidb2277@hotmail.com>
2015-03-09 10:32:47 -0700

    Improv player 2
    fix nolans nolans stuff

--- Assets/Scripts/playerControls.js
+++ Assets/Scripts/playerControls.js
@@ -1,10 +1,12 @@
 
 var velocity : Vector3 = Vector3.zero;
-var gravity : float = 50.0;
+var deceleration : float = 40;
+var gravity : float = 80.0;
 var moveRight = true;
 var running = false;
 var movingTime : float;
 var jumpSound : AudioClip;
+var jumpheight : float = 80.0;
 
 var soundRate : float = 0.0;
 var soundDelay : float = 0.0;
@@ -26,6 +28,7 @@ function Update()
 	
 	if (controller.isGrounded)
 	{
+	
 		if (Input.GetKey("left"))
 			{
 		velocity = Vector3 (-40,0,0);
@@ -63,54 +66,72 @@ function Update()
 		if (Input.GetKey("up"))
 		{
 			
-			velocity.y += 50;
-			if (moveRight)
-			{
-				aniPlay.aniSprite(16,16,5,13,4,12,false);   //jump face right
-			}
-			else
-			{
-				aniPlay.aniSprite(16,16,5,13,4,12,true);    //jump face left
-			}
+			velocity.y += jumpheight;
+				aniPlay.aniSprite(16,16,5,13,4,12,!moveRight);   //jump 
 		}		
 		else
 		{
 			if (Input.GetKey("right"))
 			{
 				if (running)
-					aniPlay.aniSprite(16,16,0,3,16,12,false);   //run face right		
+					aniPlay.aniSprite(16,16,0,3,16,12,false);   //run 		
 				else
-					aniPlay.aniSprite(16,16,0,1,10,12,false);   //walk face right		
+					aniPlay.aniSprite(16,16,0,1,10,12,false);   //walk 	
 			}
 			else if (Input.GetKey("left"))
 			{
 				if (running)
-					aniPlay.aniSprite(16,16,0,3,16,12,true);   //run face left		
+					aniPlay.aniSprite(16,16,0,3,16,12,true);   //run 
 				else
-					aniPlay.aniSprite(16,16,0,1,10,12,true);   //walk face left		
-					
+					aniPlay.aniSprite(16,16,0,1,10,12,true);   //walk		
 			}
 			else if (Input.GetKey("down"))
 			{
-				if (moveRight)
-					aniPlay.aniSprite(16,16,0,8,16,12,false);   //crouch face right		
-				else
-					aniPlay.aniSprite(16,16,0,9,16,12,false);   //crouch face left	
+					aniPlay.aniSprite(16,16,0,8,16,12,!moveRight);   //crouch 	
 			}		
 			else
-			{
-				if (moveRight)
-					aniPlay.aniSprite(16,16,0,0,16,12,false);   //neutral face right		
-				else
-					aniPlay.aniSprite(16,16,0,0,16,12,true);   //neutral face left	
+			{	
+					aniPlay.aniSprite(16,16,0,0,16,12,!moveRight);   //neutral 	
 			}
 		 }
 	}
-	if (Input.GetKey("p"))
-	Application.LoadLevel("Title Page");
-
+	else 
+	{
+		if (Input.GetKey("left"))
+		{
+			velocity += Vector3 (-2,0,0);
+			aniPlay.aniSprite(16,16,5,13,2,12,!moveRight);   //jump 
+		}
+		else if (Input.GetKey("right"))
+		{
+				velocity += Vector3 (2,0,0);
+				aniPlay.aniSprite(16,16,5,13,2,12,!moveRight);   //jump 
+		}
+		if (velocity.x > 0)
+		{
+			moveRight = true;
+		}
+		else if (velocity.x < 0)
+		{
+			moveRight = false;
+		}
+	}
+	if (velocity.x >0)
+	velocity.x  -= deceleration * Time.deltaTime;
+	else if (velocity.x <0)
+	
+	velocity.x  += deceleration * Time.deltaTime;
 	velocity.y -= gravity * Time.deltaTime;
+	
+	var checkPos : Vector3 = transform.position;
 	controller.Move(velocity * Time.deltaTime);
+	
+	if(checkPos.x == transform.position.x)
+	{
+	velocity.x =0;
+	}
+	if(checkPos.y == transform.position.y)
+	velocity.y = -velocity.y;
 }
 
 
