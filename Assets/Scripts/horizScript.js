aff135d79fb00948dec8f9f711c0fcb1a51b7fe8
MSaldana362 <msaldana362@gmail.com>
2015-03-11 09:47:55 -0700

    Final scene
    Mhmm

---
+++ Assets/Scripts/horizScript.js
@@ -0,0 +1,26 @@
+﻿
+var xdir : float = 1.0;
+var velocity : Vector3=Vector3.zero;
+
+
+function Update()
+{
+
+	//transform.position.y+=ydir;
+	velocity = Vector3(xdir, 0, 0);
+	var controller : CharacterController = GetComponent(CharacterController);
+	controller.Move(velocity*Time.deltaTime);
+	
+}
+
+
+function OnTriggerEnter(other : Collider)
+{
+	if (other.tag == "verticleCollider")
+	{
+		xdir = -xdir;
+		print("got here");
+	}
+	
+}
+
---
+++ Assets/Scripts/verticleScript.js
@@ -0,0 +1,26 @@
+﻿
+var ydir : float = 1.0;
+var velocity : Vector3=Vector3.zero;
+
+
+function Update()
+{
+
+	//transform.position.y+=ydir;
+	velocity = Vector3(0, ydir, 0);
+	var controller : CharacterController = GetComponent(CharacterController);
+	controller.Move(velocity*Time.deltaTime);
+	
+}
+
+
+function OnTriggerEnter(other : Collider)
+{
+	if (other.tag == "verticleCollider")
+	{
+		ydir = -ydir;
+		print("got here");
+	}
+	
+}
+
